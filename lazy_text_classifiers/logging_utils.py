d38e17abc4aae18e54af5252e99dbbaee2f0855a
Eva Maxfield Brown <evamaxfieldbrown@gmail.com>
2023-02-04 18:47:36 -0800

    Lint and format

--- lazy_text_classifiers/logging_utils.py
+++ lazy_text_classifiers/logging_utils.py
@@ -7,7 +7,7 @@ import re
 def set_global_logging_level(
     level: int = logging.ERROR,
     prefices: list[str] | None = None,
-):
+) -> None:
     """
     Override logging levels of different modules based on their name as a prefix.
     It needs to be invoked after the modules have been loaded so that their
@@ -31,7 +31,7 @@ def set_global_logging_level(
     if prefices is None:
         prefices = [""]
 
-    prefix_re = re.compile(fr"^(?:{ '|'.join(prefices) })")
+    prefix_re = re.compile(rf"^(?:{ '|'.join(prefices) })")
     for name in logging.root.manager.loggerDict:
         if re.match(prefix_re, name):
             logging.getLogger(name).setLevel(level)
